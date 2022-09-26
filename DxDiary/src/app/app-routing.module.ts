32829b514eb566100e5f1a9c970a2f8c6eefba5f
Andrew Glenn <andrew.glenn@orco.com.au>
2022-09-26 17:23:05 +1000

    Added new routes and edit components

--- DxDiary/src/app/app-routing.module.ts
+++ DxDiary/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/components/dashboard/dashboard.component';
+import { DialysisFormComponent } from './dialysis/components/dialysis-form/dialysis-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'dashboard', pathMatch: 'full'
+  },
+  {
+    path: 'dashboard', component: DashboardComponent
+  },
+  {
+    path: 'dialysis', component: DialysisFormComponent
+  },
+  {
+    path: 'augment', component: DialysisFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- DxDiary/src/app/app.component.ts
+++ DxDiary/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'DxDiary';
   public expanded = true;
+
+  public constructor(private _router: Router) {}
+  public navigate(path:string) {
+    this._router.navigateByUrl(path);
+  }
 }
